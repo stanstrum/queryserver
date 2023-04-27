7b150eb6db9defe90b9fbce70a5f4c4da183f769
Stan Strum <stanleystrum@gmail.com>
2023-04-26 21:41:03 -0700

    Remove `lib/` guff

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { QuerierParameters } from "@/Options";
-import { QueryData } from "./Queriers";
-type QueryServerResults = QueryData & {
-    ip: string;
-    type: "Java" | "Bedrock" | "Unknown" | "Crossplay";
-};
-declare function queryServer(...args: QuerierParameters): Promise<QueryServerResults>;
-export default queryServer;
