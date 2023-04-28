c679c85a971a3c1162623bd4d6797362b6fdb030
Stan Strum <stanleystrum@gmail.com>
2023-04-28 12:29:00 -0700

    Add'l types, SchemaMap using Zod

--- src/PacketTool/DataType.ts
+++ src/PacketTool/DataType.ts
@@ -1,27 +1,44 @@
 import z, { ZodTypeAny } from "zod";
 
 export enum DataType {
+  uint8,
   uint16be,
+  uint32be,
+  uint64be,
   Bytes,
   VarInt,
   VarIntString,
+  ShortString, // Uses uint16be for size pfx.
+  RaknetMagic,
 };
 
+/** @todo replace these magic numbers with something more intuitive */
 export const SchemaMap = {
+  [DataType.uint8]: z.number()
+    .nonnegative().int()
+    .lte(255),
   [DataType.uint16be]: z.number()
-    .int()
-    .gte(0)
-    .lte(1),
+    .nonnegative().int()
+    .lte(65535),
+  [DataType.uint32be]: z.number()
+    .nonnegative().int()
+    .lte(4294967295),
+  [DataType.uint64be]: z.bigint()
+    .nonnegative()
+    .lte(18446744073709551615n),
   [DataType.Bytes]: z.instanceof(Buffer),
   [DataType.VarInt]: z.number()
     .int()
     .gte(-2147483648)
     .lte(2147483647),
   [DataType.VarIntString]: z.string(),
-} as const;
+  [DataType.ShortString]: z.string(),
+  [DataType.RaknetMagic]: z.literal(true),
+} as const satisfies Record<DataType, ZodTypeAny>;
 
+type SchemaMap = typeof SchemaMap;
 export type TypeMap = {
-  -readonly [K in keyof (typeof SchemaMap)]: z.infer<(typeof SchemaMap)[K]>;
+  -readonly [K in keyof SchemaMap]: z.infer<SchemaMap[K]>;
 };
 
 export type DeserializerOptions = {
