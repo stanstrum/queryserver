85b60f26e64d04a0d57e36b5175079b5450fc961
Stan Strum <stanleystrum@gmail.com>
2023-04-28 12:28:36 -0700

    Change quotes

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+/** @type {import("ts-jest").JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   moduleNameMapper: {
     "@/(.*)": "<rootDir>/src/$1"
   },
